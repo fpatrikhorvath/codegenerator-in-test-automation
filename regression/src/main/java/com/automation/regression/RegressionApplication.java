239a34a7023e0559aac821de1960bd84d9cda3c6
Patrik Horvath <ferencpatrikhorvath@gmail.com>
2024-05-17 11:21:08 +0200

    Server and swagger impl Spring init

---
+++ regression/src/main/java/com/automation/regression/RegressionApplication.java
@@ -0,0 +1,13 @@
+package com.automation.regression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegressionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegressionApplication.class, args);
+	}
+
+}
---
+++ regression/src/test/java/com/automation/regression/RegressionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automation.regression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegressionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
